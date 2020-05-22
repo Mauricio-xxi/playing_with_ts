8bb211158f266d133a387fe31675add0f584424f
Acamacho <a.camacho@cadt.com>
2020-05-23 00:14:16 +0200

    javascript transpile from ts

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 exports.__esModule = true;
 console.log("HELLO WORLD");
 // types
@@ -45,4 +58,36 @@ var myTodo = { title: "lorem title", text: " lorem text" };
 function showTodo(todo) {
     console.log(todo.title + " - " + todo.text);
 }
-// clases 
+// clases
+var User = /** @class */ (function () {
+    function User(name, email, age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+    User.prototype.register = function () {
+        console.log(this.name + " is registered");
+    };
+    User.prototype.showMeAge = function () {
+        console.log(this.age + " years old");
+        return this.age + " years old";
+    };
+    User.prototype.payInvoice = function () {
+        console.log(this.name + " has paid");
+    };
+    return User;
+}());
+var Member = /** @class */ (function (_super) {
+    __extends(Member, _super);
+    function Member(id, name, email, age) {
+        var _this = _super.call(this, name, email, age) || this;
+        _this.id = id;
+        return _this;
+    }
+    Member.prototype.payInvoice = function () {
+        _super.prototype.payInvoice.call(this);
+    };
+    return Member;
+}(User));
+var alvaro = new User("alvaro", "alvaro@alvaro.com", 28);
+var manolo = new Member(1, "Manolo", "manolo@a.com", 25);
